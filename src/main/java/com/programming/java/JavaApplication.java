4c43b4a9074cea110dee2865f3b166f816d728ab
Nikola <nikj1705@gmail.com>
2022-03-14 23:03:01 +0200

    added ChapterTwo

--- src/main/java/com/programming/java/JavaApplication.java
+++ src/main/java/com/programming/java/JavaApplication.java
@@ -1,6 +1,7 @@
 package com.programming.java;
 
 import com.programming.java.chapter1.ChapterOne;
+import com.programming.java.chapter2.ChapterTwo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,6 +10,8 @@ public class JavaApplication {
 	public static void main(String[] args) {
 		ChapterOne chapterOne = new ChapterOne();
 		chapterOne.printAll();
+		ChapterTwo chapterTwo = new ChapterTwo();
+		chapterTwo.printAll();
 	}
 
 }
