b143f6ce552eb11eea7786e3b492c9ea963ea3cf
Nikola <nikj1705@gmail.com>
2022-05-02 18:15:36 +0300

    Chapter Eleven complete

--- src/main/java/com/programming/java/JavaApplication.java
+++ src/main/java/com/programming/java/JavaApplication.java
@@ -1,11 +1,5 @@
 package com.programming.java;
 
-import com.programming.java.chapter10.ChapterTen;
-import com.programming.java.chapter11.ChapterEleven;
-import com.programming.java.chapter7.ChapterSeven;
-import com.programming.java.chapter8.ChapterEight;
-import com.programming.java.chapter9.ChapterNine;
-
 public class JavaApplication {
 
     public static void main(String[] args) {
@@ -24,19 +18,7 @@ public class JavaApplication {
 //        ChapterEight.printAll();
 //        ChapterNine.printAll();
 //        ChapterTen.printAll();
-//        ChapterEleven.taskOne();
-//        ChapterEleven.taskTwo();
-//        ChapterEleven.taskThree();
-//        ChapterEleven.taskFour();
-//        ChapterEleven.taskSix();
-//        ChapterEleven.taskSeven();
-//        ChapterEleven.taskEight();
-//        ChapterEleven.taskNine();
-//        ChapterEleven.taskTen();
-        ChapterEleven.taskEleven();
-        ChapterEleven.taskTwelve();
-        ChapterEleven.taskThirteen();
-        ChapterEleven.taskFourteen();
+//        ChapterEleven.printAll();
     }
 
 }
--- src/main/java/com/programming/java/chapter11/ChapterEleven.java
+++ src/main/java/com/programming/java/chapter11/ChapterEleven.java
@@ -2,6 +2,7 @@ package com.programming.java.chapter11;
 
 import com.programming.java.chapter10.Circle2D;
 import com.programming.java.chapter10.MyDate;
+import com.programming.java.chapter10.MyPoint;
 import com.programming.java.chapter9.Account;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class ChapterEleven {
 
         System.out.println("Triangle: " + triangle);
     }
+
     public static void taskTwo() {
         Person person = new Person();
         person.setName("Person");
@@ -269,4 +271,207 @@ public class ChapterEleven {
         ArrayList<Integer> combined = union(list1, list2);
         System.out.println(combined);
     }
+
+    public static double areaConvexPolygon(ArrayList<MyPoint> points) {
+        int N = points.size();
+
+        double leftSide = 0, rightSide = 0, area = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (i == N - 1) {
+                leftSide += points.get(i).getX() * points.get(0).getY();
+                rightSide += points.get(i).getY() * points.get(0).getX();
+            } else {
+                leftSide += points.get(i).getX() * points.get(i + 1).getY();
+                rightSide += points.get(i).getY() * points.get(i + 1).getX();
+            }
+        }
+
+        return Math.abs((1.0 / 2.0) * (leftSide - rightSide));
+    }
+
+    public static void taskFifteen() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of points: ");
+        int n = scanner.nextInt();
+
+        ArrayList<MyPoint> points = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Point [%d/%d]\n", i + 1, n);
+            System.out.print("Enter x: ");
+            double x = scanner.nextDouble();
+            System.out.print("Enter y: ");
+            double y = scanner.nextDouble();
+            points.add(new MyPoint(x, y));
+        }
+
+        System.out.println("The total area is: " + areaConvexPolygon(points));
+    }
+
+    public static void taskSixteen() {
+        final int NUMBER_OF_QUESTIONS = 5; // Number of questions
+        int correctCount = 0; // Count the number of correct answers
+        int count = 0; // Count the number of questions
+        long startTime = System.currentTimeMillis();
+        String output = " "; // output string is initially empty
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Integer> previousAnswers = new ArrayList<>();
+
+        // 1. Generate two random single-digit integers
+        int number1 = (int) (1 + Math.random() * 14);
+        int number2 = (int) (1 + Math.random() * 14);
+
+        while (count < NUMBER_OF_QUESTIONS) {
+
+
+            // 3. Prompt the student to answer "What is number1 + number2?"
+            System.out.print(
+                    "What is " + number1 + " + " + number2 + "? ");
+            int answer = input.nextInt();
+
+            // 4. Grade the answer and display the result
+            if (number1 + number2 == answer) {
+                System.out.println("You are correct!");
+                previousAnswers = new ArrayList<>();
+                correctCount++; // Increase the correct answer count
+                // Increase the question count
+                count++;
+                output += "\n" + number1 + "+" + number2 + "=" + answer + ((number1 + number2 == answer) ? " correct" : " wrong");
+                // Generate new question
+                number1 = (int) (1 + Math.random() * 14);
+                number2 = (int) (1 + Math.random() * 14);
+            } else {
+                if (previousAnswers.contains(answer)) {
+                    System.out.println("You already entered " + answer);
+                } else {
+                    previousAnswers.add(answer);
+                    System.out.println("Your answer is wrong. Try again!");
+                }
+            }
+        }
+
+        long endTime = System.currentTimeMillis();
+        long testTime = endTime - startTime;
+
+        System.out.println("Correct count is " + correctCount +
+                "\nTest time is " + testTime / 1000 + " seconds\n" + output);
+    }
+
+    public static ArrayList<Integer> getFactors(int m) {
+        ArrayList<Integer> factors = new ArrayList<>();
+        int divisor = 2;
+        while (m != 1) {
+            if (m % divisor == 0) {
+                factors.add(divisor);
+                m /= divisor;
+            } else {
+                divisor += 1;
+            }
+        }
+
+        return factors;
+    }
+
+    public static ArrayList<Integer> getOddFactors(ArrayList<Integer> factors) {
+        ArrayList<Integer> odds = new ArrayList<>();
+
+        for (int factor : factors) {
+            int count = 0;
+            for (int f : factors) {
+                if (f == factor)
+                    count += 1;
+            }
+            if (count % 2 != 0)
+                odds.add(factor);
+        }
+
+        removeDuplicate(odds);
+        return odds;
+    }
+
+    public static void taskSeventeen() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer M: ");
+        int M = scanner.nextInt();
+
+        ArrayList<Integer> factors = getFactors(M);
+        ArrayList<Integer> oddFactors = getOddFactors(factors);
+
+        int N = 1;
+        for (int factor : oddFactors)
+            N *= factor;
+
+        System.out.println("The smallest number N for M*N to be a perfect square is " + N + ", M*N is " + (M * N));
+    }
+
+    public static ArrayList<Character> toCharacterArray(String s) {
+        ArrayList<Character> asList = new ArrayList<>();
+        for (char c : s.toCharArray())
+            asList.add(c);
+
+        return asList;
+    }
+
+    public static void taskEighteen() {
+        System.out.println(toCharacterArray("abc"));
+    }
+
+    public static void taskNineteen() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of objects: ");
+        int N = scanner.nextInt();
+
+        ArrayList<Integer> weights = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            System.out.printf("Enter weight of object [%d/%d]: ", i + 1, N);
+            weights.add(scanner.nextInt());
+        }
+
+        ArrayList<Integer> containers = new ArrayList<>();
+        containers.add(10);
+        ArrayList<ArrayList<Integer>> elements = new ArrayList<>();
+        elements.add(new ArrayList<>());
+        int containersCount = 0;
+
+        for (int weight : weights) {
+            int availableSpace = containers.get(containersCount);
+            if (availableSpace < weight) {
+                // not enough space
+                containers.add(10 - weight);
+                elements.add(new ArrayList<>());
+                containersCount += 1;
+                elements.get(containersCount).add(weight);
+            } else {
+                containers.set(containersCount, availableSpace - weight);
+                elements.get(containersCount).add(weight);
+            }
+        }
+
+        for (int i = 0; i < containersCount; i++) {
+            System.out.println("Container " + (i + 1) + " contains objects with weight: " + elements.get(i));
+        }
+    }
+
+    public static void printAll() {
+        ChapterEleven.taskOne();
+        ChapterEleven.taskTwo();
+        ChapterEleven.taskThree();
+        ChapterEleven.taskFour();
+        ChapterEleven.taskSix();
+        ChapterEleven.taskSeven();
+        ChapterEleven.taskEight();
+        ChapterEleven.taskNine();
+        ChapterEleven.taskTen();
+        ChapterEleven.taskEleven();
+        ChapterEleven.taskTwelve();
+        ChapterEleven.taskThirteen();
+        ChapterEleven.taskFourteen();
+        ChapterEleven.taskFifteen();
+        ChapterEleven.taskSixteen();
+        ChapterEleven.taskSeventeen();
+        ChapterEleven.taskEighteen();
+        ChapterEleven.taskNineteen();
+    }
+
 }
