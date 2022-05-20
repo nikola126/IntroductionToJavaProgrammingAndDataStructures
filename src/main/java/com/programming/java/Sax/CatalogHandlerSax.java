3b4040328a30ccd1901a5ad3048588f6777dbc19
Nikola126 <nikj1705@gmail.com>
2022-05-20 10:04:47 +0300

    SAX parsing implementation

---
+++ src/main/java/com/programming/java/Sax/CatalogHandlerSax.java
@@ -0,0 +1,142 @@
+package com.programming.java.Sax;
+
+import com.programming.java.Jackson.Catalog.Author;
+import com.programming.java.Jackson.Catalog.Book;
+import com.programming.java.Jackson.Catalog.Catalog;
+import com.programming.java.Jackson.Catalog.Price;
+import com.programming.java.Jackson.CustomDateDeserializer;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogHandlerSax extends DefaultHandler {
+    // use the StringBuilder to keep tag contents ("buffer")
+    private StringBuilder elementValue = new StringBuilder();
+
+    // define expected Java objects
+    Catalog catalog;
+    Book book;
+    List<Book> bookList;
+    Author author;
+    List<Author> authorList;
+    Price price;
+
+    // define expected XML tags/properties
+    private final String CATALOG = "catalog";
+    private final String BOOK = "book";
+    private final String ID = "id";
+    private final String COVER = "cover";
+    private final String AUTHORS = "authors";
+    private final String AUTHOR = "author";
+    private final String FIRSTNAME = "first_name";
+    private final String LASTNAME = "last_name";
+    private final String TITLE = "title";
+    private final String GENRE = "genre";
+    private final String PRICE = "price";
+    private final String CURRENCY = "currency";
+    private final String PUBLISHDATE = "publish_date";
+    private final String DESCRIPTION = "description";
+
+    public Catalog getCatalog() {
+        return catalog;
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        // initialize nested objects
+        catalog = new Catalog();
+        book = new Book();
+        bookList = new ArrayList<>();
+        author = new Author();
+        authorList = new ArrayList<>();
+        price = new Price();
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        // clear the stringBuilder
+        elementValue.setLength(0);
+
+        // extract XML properties and call setters
+        switch (qName.toLowerCase()) {
+            case CATALOG: {
+                catalog = new Catalog();
+                break;
+            }
+            case BOOK: {
+                book = new Book();
+                book.setId(attributes.getValue(ID));
+                book.setCover(attributes.getValue(COVER));
+                break;
+            }
+            case AUTHORS: {
+                authorList = new ArrayList<>();
+                break;
+            }
+            case PRICE: {
+                price.setCurrency(attributes.getValue(CURRENCY));
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        // call setters with the current values in the stringBuilder
+        switch (qName.toLowerCase()) {
+            case CATALOG: {
+                catalog.setBooks(bookList);
+                break;
+            }
+            case BOOK: {
+                bookList.add(book);
+                return;
+            }
+            case FIRSTNAME: {
+                author.setFirstName(elementValue.toString());
+                break;
+            }
+            case LASTNAME: {
+                author.setLastName(elementValue.toString());
+                break;
+            }
+            case AUTHOR: {
+                authorList.add(author);
+                break;
+            }
+            case AUTHORS: {
+                book.setAuthorsList(authorList);
+                break;
+            }
+            case TITLE: {
+                book.setTitle(elementValue.toString());
+                break;
+            }
+            case GENRE: {
+                book.setGenre(elementValue.toString());
+                break;
+            }
+            case PRICE: {
+                price.setAmount(Double.parseDouble(elementValue.toString()));
+                book.setPrice(price);
+                break;
+            }
+            case PUBLISHDATE: {
+                book.setPublishDate(CustomDateDeserializer.dateParser(elementValue.toString()));
+                break;
+            }
+            case DESCRIPTION: {
+                book.setDescription(elementValue.toString());
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        elementValue.append(ch, start, length);
+    }
+}
---
+++ src/main/java/com/programming/java/Sax/saxSerialization.java
@@ -0,0 +1,40 @@
+package com.programming.java.Sax;
+
+import com.programming.java.Jackson.Catalog.Catalog;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+
+public class saxSerialization {
+    public static void main(String[] args) {
+        CatalogHandlerSax catalogHandlerSax = new CatalogHandlerSax();
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        SAXParser saxParser;
+
+        try {
+            saxParser = saxParserFactory.newSAXParser();
+        } catch (SAXException | ParserConfigurationException e) {
+            System.out.println("Error creating SAX Parser!");
+            return;
+        }
+
+        String xmlFileLocation = "./src/main/resources/xmlFiles/ms_books.xml";
+        Catalog catalog;
+
+        try {
+            saxParser.parse(xmlFileLocation, catalogHandlerSax);
+        } catch (IOException e) {
+            System.out.println("Error reading file " + xmlFileLocation);
+            return;
+        } catch (SAXException e) {
+            System.out.println("Error parsing the file " + xmlFileLocation);
+            return;
+        }
+
+        catalog = catalogHandlerSax.getCatalog();
+        System.out.println(catalog);
+    }
+}
