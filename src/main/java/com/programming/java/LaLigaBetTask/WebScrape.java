03773a23c8f97cf384347e0975e08ba1426783ac
Nikola126 <nikj1705@gmail.com>
2022-05-27 11:56:24 +0300

    added gamesPlayed in team, removed duplicate rounds

--- src/main/java/com/programming/java/LaLigaBetTask/WebScrape.java
+++ src/main/java/com/programming/java/LaLigaBetTask/WebScrape.java
@@ -15,6 +15,11 @@ import java.util.*;
 
 public class WebScrape {
     public static void main(String[] args) {
+        League league = parseLaLiga();
+        System.out.println(league);
+    }
+
+    public static League parseLaLiga() {
         // LaLiga Results Tab
         final String URL = "https://www.betexplorer.com/soccer/spain/laliga/results/";
 
@@ -29,14 +34,14 @@ public class WebScrape {
         // Initialize fields
         League league = new League();
         Set<Team> teamSet = new TreeSet<>(new CustomTeamComparator());
-        Round currentRound = new Round();
+        // rounds are not consecutive, map is used for easier insertions
+        Map<Integer, List<Game>> roundMap = new HashMap<>();
         List<Game> gameList = new ArrayList<>();
         List<Round> roundList = new ArrayList<>();
         Game currentGame;
 
         try {
             final Document document = Jsoup.connect(URL).get();
-
 
             // isolate League Name and Years
             for (Element row : document.select(selectorLeagueTitleAndYear)) {
@@ -72,16 +77,12 @@ public class WebScrape {
                     // isolate round row
 
                     if (currentRoundNumber != 0) {
-                        // add the completed round to the list
-                        currentRound.setNumber(currentRoundNumber);
-                        currentRound.setGameList(gameList);
-                        roundList.add(currentRound);
-
-                        currentRound = new Round();
-                        gameList = new ArrayList<>();
+                        // add the completed round to the map
+                        roundMap.put(currentRoundNumber, gameList);
                     }
 
                     currentRoundNumber = Integer.parseInt(row.text().split("\\.")[0]);
+                    gameList = roundMap.getOrDefault(currentRoundNumber, new ArrayList<>());
                     continue;
                 }
 
@@ -127,6 +128,14 @@ public class WebScrape {
 
                 }
 
+                GameResult currentGameResult;
+                if (scoreTeamHome > scoreTeamAway)
+                    currentGameResult = GameResult.HOME;
+                else if (scoreTeamHome < scoreTeamAway)
+                    currentGameResult = GameResult.AWAY;
+                else
+                    currentGameResult = GameResult.DRAW;
+
                 // Add game to the list
                 currentGame.setNameTeamHome(nameTeamHome);
                 currentGame.setNameTeamAway(nameTeamAway);
@@ -136,111 +145,120 @@ public class WebScrape {
                 currentGame.setOddsX(oddsArray[1]);
                 currentGame.setOdds2(oddsArray[2]);
                 currentGame.setGameDate(gameDate);
-
-                GameResult currentGameResult;
-                if (scoreTeamHome > scoreTeamAway)
-                    currentGameResult = GameResult.HOME;
-                else if (scoreTeamHome < scoreTeamAway)
-                    currentGameResult = GameResult.AWAY;
-                else
-                    currentGameResult = GameResult.DRAW;
-
                 currentGame.setGameResult(currentGameResult);
 
                 gameList.add(currentGame);
 
-                // TODO Extract in method, CLEAN PLS
-                // Update Team statistics
-                // Update Team statistics - Home team
-                Iterator<Team> iterator = teamSet.iterator();
-                Team teamInSet = null;
-                boolean setContainsTeam = false;
-
-                Team homeTeam = new Team();
-                homeTeam.setName(nameTeamHome);
-
-                while (iterator.hasNext()) {
-                    teamInSet = iterator.next();
-
-                    if (Objects.equals(teamInSet.getName(), homeTeam.getName())) {
-                        homeTeam.setHomeWins(teamInSet.getHomeWins());
-                        homeTeam.setDraws(teamInSet.getDraws());
-                        homeTeam.setAwayWins(teamInSet.getAwayWins());
-                        // Team is already in the set
-                        if (currentGameResult.equals(GameResult.HOME))
-                            homeTeam.setHomeWins(teamInSet.getHomeWins() + 1);
-                        else if (currentGameResult.equals(GameResult.DRAW))
-                            homeTeam.setDraws(teamInSet.getDraws() + 1);
-
-                        setContainsTeam = true;
-                        break;
-                    }
-                }
-                if (!setContainsTeam) {
-                    // add team to the set
-                    if (currentGameResult.equals(GameResult.HOME))
-                        homeTeam.setHomeWins(1);
-                    else if (currentGameResult.equals(GameResult.DRAW))
-                        homeTeam.setDraws(1);
-                    teamSet.add(homeTeam);
-                } else {
-                    teamSet.remove(teamInSet);
-                    teamSet.add(homeTeam);
-                }
-
-                // Update Team statistics - Away team
-                // TODO this avoids ConcurrentModificationException, unnecessary?
-                iterator = teamSet.iterator();
-                teamInSet = new Team();
-
-                Team awayTeam = new Team();
-                awayTeam.setName(nameTeamAway);
-
-                setContainsTeam = false;
-
-                while (iterator.hasNext()) {
-                    teamInSet = iterator.next();
-
-                    if (Objects.equals(teamInSet.getName(), awayTeam.getName())) {
-                        awayTeam.setHomeWins(teamInSet.getHomeWins());
-                        awayTeam.setDraws(teamInSet.getDraws());
-                        awayTeam.setAwayWins(teamInSet.getAwayWins());
-                        // Team is already in the set
-                        if (currentGameResult.equals(GameResult.AWAY))
-                            awayTeam.setAwayWins(teamInSet.getAwayWins() + 1);
-                        else if (currentGameResult.equals(GameResult.DRAW))
-                            awayTeam.setDraws(teamInSet.getDraws() + 1);
-
-                        setContainsTeam = true;
-                        break;
-                    }
-                }
-
-                if (!setContainsTeam) {
-                    // add team to set
-                    if (currentGameResult.equals(GameResult.AWAY))
-                        awayTeam.setAwayWins(1);
-                    else if (currentGameResult.equals(GameResult.DRAW))
-                        awayTeam.setDraws(1);
-                    teamSet.add(awayTeam);
-                } else {
-                    teamSet.remove(teamInSet);
-                    teamSet.add(awayTeam);
-                }
+                updateTeamStatistics(teamSet, nameTeamHome, nameTeamAway, currentGameResult);
             }
 
-            // add the completed round to the list
+            // add the completed round to the map
             if (gameList.size() != 0) {
-                currentRound.setNumber(currentRoundNumber);
-                currentRound.setGameList(gameList);
-                roundList.add(currentRound);
+                roundMap.put(currentRoundNumber, gameList);
             }
         } catch (Selector.SelectorParseException | IOException e) {
             e.printStackTrace();
         }
 
+        // convert map to list
+        roundList = new ArrayList<>();
+        for (Map.Entry<Integer, List<Game>> rounds : roundMap.entrySet()) {
+            Round currentRound = new Round();
+            currentRound.setNumber(rounds.getKey());
+            currentRound.setGameList(rounds.getValue());
+            roundList.add(currentRound);
+        }
         league.setRounds(roundList);
         league.setTeams(teamSet);
-        System.out.println(league);
+
+        return league;
+    }
+
+    public static void updateTeamStatistics(Set<Team> teamSet, String nameTeamHome, String nameTeamAway,
+                                            GameResult currentGameResult) {
+        // Update Team statistics - Home team
+        Iterator<Team> iterator = teamSet.iterator();
+        Team teamInSet = null;
+        boolean setContainsTeam = false;
+
+        Team homeTeam = new Team();
+        homeTeam.setName(nameTeamHome);
+
+        while (iterator.hasNext()) {
+            teamInSet = iterator.next();
+
+            if (Objects.equals(teamInSet.getName(), homeTeam.getName())) {
+                // Team is already in the set, update statistics
+                homeTeam.setGamesPlayed(teamInSet.getGamesPlayed() + 1);
+                homeTeam.setHomeWins(teamInSet.getHomeWins());
+                homeTeam.setDraws(teamInSet.getDraws());
+                homeTeam.setAwayWins(teamInSet.getAwayWins());
+
+                if (currentGameResult.equals(GameResult.HOME))
+                    homeTeam.setHomeWins(teamInSet.getHomeWins() + 1);
+                else if (currentGameResult.equals(GameResult.DRAW))
+                    homeTeam.setDraws(teamInSet.getDraws() + 1);
+
+                setContainsTeam = true;
+                break;
+            }
+        }
+        if (setContainsTeam) {
+            // update team in set
+            teamSet.remove(teamInSet);
+            teamSet.add(homeTeam);
+        } else {
+            // add team to the set
+            homeTeam.setGamesPlayed(1);
+            if (currentGameResult.equals(GameResult.HOME))
+                homeTeam.setHomeWins(1);
+            else if (currentGameResult.equals(GameResult.DRAW))
+                homeTeam.setDraws(1);
+            teamSet.add(homeTeam);
+        }
+
+        // Update Team statistics - Away team
+        // TODO this avoids ConcurrentModificationException, unnecessary?
+        iterator = teamSet.iterator();
+        teamInSet = null;
+
+        Team awayTeam = new Team();
+        awayTeam.setName(nameTeamAway);
+
+        setContainsTeam = false;
+
+        while (iterator.hasNext()) {
+            teamInSet = iterator.next();
+
+            if (Objects.equals(teamInSet.getName(), awayTeam.getName())) {
+                // Team is already in the set, update statistics
+                awayTeam.setGamesPlayed(teamInSet.getGamesPlayed() + 1);
+                awayTeam.setHomeWins(teamInSet.getHomeWins());
+                awayTeam.setDraws(teamInSet.getDraws());
+                awayTeam.setAwayWins(teamInSet.getAwayWins());
+
+                if (currentGameResult.equals(GameResult.AWAY))
+                    awayTeam.setAwayWins(teamInSet.getAwayWins() + 1);
+                else if (currentGameResult.equals(GameResult.DRAW))
+                    awayTeam.setDraws(teamInSet.getDraws() + 1);
+
+                setContainsTeam = true;
+                break;
+            }
+        }
+
+        if (setContainsTeam) {
+            // update team in set
+            teamSet.remove(teamInSet);
+            teamSet.add(awayTeam);
+        } else {
+            // add team to the set
+            awayTeam.setGamesPlayed(1);
+            if (currentGameResult.equals(GameResult.AWAY))
+                awayTeam.setAwayWins(1);
+            else if (currentGameResult.equals(GameResult.DRAW))
+                awayTeam.setDraws(1);
+            teamSet.add(awayTeam);
+        }
     }
 }
